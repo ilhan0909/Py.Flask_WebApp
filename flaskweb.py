45015222ad40517e7ea26fb47dc8650f435ff36c
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-06 14:51:34 +0300

    added mock blog posts

--- flaskweb.py
+++ flaskweb.py
@@ -3,14 +3,30 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'İlhan Bayramoğlu',
+        'title' : 'First Blog Post',
+        'content' : 'Content of First Blog Post',
+        'date_posted' : 'March 5, 2022'
+    },
+    {
+        'author' : 'Rabia',
+        'title' : 'Second Blog Post',
+        'content' : 'Content of Second Blog Post',
+        'date_posted' : 'March 6, 2022'
+    }
+
+]
+
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
