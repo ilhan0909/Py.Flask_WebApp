6999d4a4b5b86f50cf499a67c57ce86bebdef10e
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-06 11:23:27 +0300

    first commit on flaskweb project

---
+++ flaskweb.py
@@ -0,0 +1,16 @@
+##start of flask framework webapp##
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return '<h1>Welcome to Home Page!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>Welcome to About Page!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
