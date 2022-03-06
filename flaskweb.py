65fe73e0cdfcc449baec21e806250134db3fdfff
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-06 13:05:59 +0300

    added html files and called pages from them

--- flaskweb.py
+++ flaskweb.py
@@ -1,16 +1,16 @@
 ##start of flask framework webapp##
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def homepage():
-    return '<h1>Welcome to Home Page!</h1>'
+    return render_template("home.html")
 
 @app.route('/about')
 def about():
-    return '<h1>Welcome to About Page!</h1>'
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
