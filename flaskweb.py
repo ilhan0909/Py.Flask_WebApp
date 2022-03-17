ebb53212e80770e1d4fa0f5cb1f6ee4a32886755
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-18 01:14:45 +0300

    created mock info for testing out login success and flash messages

--- flaskweb.py
+++ flaskweb.py
@@ -1,8 +1,8 @@
 ##start of flask framework webapp##
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '3facec850363f99109512c58511bf1e7'
 
 posts = [
@@ -22,23 +22,33 @@ posts = [
 ]
 
 @app.route('/')
+
 @app.route('/home')
-def homepage():
+def home():
     return render_template("home.html", posts=posts)
 
 @app.route('/about')
 def about():
     return render_template("about.html", title="About")
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
-    return render_template("register.html", title="Register", form="form")
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    return render_template("login.html", title="Login", form="form")
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash('You have been logged in! ', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed,check your info!', 'danger')
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
