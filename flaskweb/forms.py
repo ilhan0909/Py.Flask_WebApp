8ca0b3568cb1b5e17c5a51c67f0e14ba3ae6ea1b
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-20 23:14:56 +0300

    added validation check for login credentials

--- flaskweb/forms.py
+++ flaskweb/forms.py
@@ -1,13 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskweb.models import User
 
 class RegistrationForm(FlaskForm):
+
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, please choose different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('That email is already have a registered account, please write different email.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
