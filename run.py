84192f55955acf67194ca7fb1941b751942c328f
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-28 15:00:28 +0300

    minor create app change

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 ##(was)start of flask framework webapp##its now a run.py file only##
-from flaskweb import app
+from flaskweb import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
