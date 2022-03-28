e2f5881b952b4e04542b3bbcc372d50aaad54812
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-28 14:30:51 +0300

    minor config change

--- flaskweb/config.py
+++ flaskweb/config.py
@@ -1,7 +1,6 @@
 import os
 class Config:
-    ##SECRET_KEY = '3facec850363f99109512c58511bf1e7'
-    ##SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
@@ -12,5 +11,6 @@ class Config:
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-print (os.environ.get("EMAIL_USER"))
-print (os.environ.get("EMAIL_PASS"))
+##(tried to see if i get envs right##
+#print (os.environ.get("EMAIL_USER"))
+#print (os.environ.get("EMAIL_PASS"))
