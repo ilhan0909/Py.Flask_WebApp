1c13d491407b720a249f0f62119c04da2082bd33
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-03-28 15:00:09 +0300

    changed app to current_app as consequence of new app creation way

--- flaskweb/users/utils.py
+++ flaskweb/users/utils.py
@@ -1,7 +1,7 @@
 import secrets, os
 from PIL import Image
-from flask import url_for
-from flaskweb import app, mail
+from flask import url_for, current_app
+from flaskweb import mail
 from flask_mail import Message
 
 
@@ -9,7 +9,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     f_name, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
 ##image file will be rescaled to 125*125 for data/storage saving purposes##
     output_size = (125, 125)
